a518ad6eb0d800e0144918525015273297fd776a
Stanislav Humeniuk <stanislav.humeniuk@outlook.com>
2024-11-25 23:25:12 +0200

    1

--- lab10/server.js
+++ lab10/server.js
@@ -5,7 +5,7 @@ const VALID_CREDENTIALS = { 'user1': 'password1', 'user2': 'password2' };
 
 function authenticate(credentials) {
     return VALID_CREDENTIALS[credentials.login] === credentials.password
-        ? `Hi Stanislav! You are in system.\n`
+        ? `Hi ${credentials.login}! You are in system.\n`
         : 'Error: Wrong login/password combination.\n';
 }
 
@@ -19,7 +19,8 @@ if (isMainThread) {
             socket.on('data', data => {
                 buffer += data.toString();
                 if (buffer.includes('\n')) {
-                    socket.end(authenticate(JSON.parse(buffer)));
+                    console.log('Request:', buffer);
+                    socket.write(authenticate(JSON.parse(buffer)));
                 }
             });
             return;
@@ -33,13 +34,14 @@ if (isMainThread) {
                 buffer = '';
             }
         });
-        worker.on('message', response => socket.end(response));
+        worker.on('message', response => socket.write(response));
         socket.on('end', () => worker.terminate());
 
     }).listen(3000, () => console.log(`Server running on port 3000 in ${parallel ? 'parallel' : 'sequential'} mode`));
 } else {
     parentPort.on('message', data => {
         try {
+            console.log('Request:', data);
             parentPort.postMessage(authenticate(JSON.parse(data)));
         } catch (err) {
             parentPort.postMessage(`Error: ${err.message}\n`);
